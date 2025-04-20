33ba6fd5cd84effc470f7c0b323f5d3b0cd8fc26
anushachaudhary <104192567+anushachaudhary@users.noreply.github.com>
2025-04-20 05:57:25 +0000

    hhs

---
+++ 05_iteration/three.js
@@ -0,0 +1,40 @@
+// for of
+
+//["","",""]
+//[{},{},{}]
+/* const arr = [1,2,3,4,5]
+for (const num of arr){
+    console.log(num);
+
+}
+
+const greeting ="Hello world"
+for (const greet of greeting){
+    console.log('Each char is ${greet}')
+}
+
+
+// Maps
+
+const map =new Map()
+map.set('IN',"India")
+map.set('ne',"nepal")
+map.set('ch',"china")
+map.set('In',"india")
+
+console.log(map);
+for (const [key, value] of map)
+{
+    console.log(key,':-', value);
+}
+
+*/
+
+const myObject={
+    'game1':'NFS',
+    'game2':'Spiderman'
+}
+//for (const [key, value] of myObject){
+  //  console.log(key,':-', value);
+
+//}
